4bbef35e97cf89a7252010c4678183be3bf900fe
geetesh <gtsh321@gmail.com>
2024-06-25 23:03:59 +0530

    added the post api end point and created a service method adding employee details from the form into Db and added method in create-employee component for calling the service method and redirecting the page to employee component

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { error } from 'console';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
@@ -10,10 +13,25 @@ export class CreateEmployeeComponent implements OnInit{
   
   employee:Employee = new Employee();
  
-  constructor(){}
+  constructor(private employeeService : EmployeeService, private router : Router){}
   ngOnInit(): void {
   }
+
+  saveEmployee(employee: Employee){
+    this.employeeService.createEmployee(this.employee).subscribe(data=>{
+      console.log(data);
+      this.goToEmployeeList();
+    },
+    error=> console.log(error)
+  );
+  }
+
+  goToEmployeeList(){
+    this.router.navigate(['/employees']);
+  }
+
   onSubmit() {
     console.log(this.employee);
+    this.saveEmployee(this.employee);
     }
 }
